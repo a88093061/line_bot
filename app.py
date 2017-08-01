a1c372241590f8f95a64dc8434278e1a27b94e38
Apie0419 <xu3mp6xjp6@gmail.com>
2017-08-01 16:04:17 +0800

    first edit

---
+++ app.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+from __future__ import unicode_literals
+
+import os
+import sys
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+# get channel_secret and channel_access_token from your environment variable
+line_bot_api = LineBotApi('gbv3ravWElm84LWb0JuaHHUYAGkfHmkE3l3CDpgrJlbjuK6qQB31vbV40u7Nf5UvzFx61Re1AascPJCrYNW+qjAL6E+iwQze4dBGhPxzn5UCQc0DWsqPWuksLPK3cj6lIVGnfKe/Bo20JlGOdpIGogdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('	
+6df6064f5f425bb0a7988c0c1a4fa7c1')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
+    return 'OK'
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
