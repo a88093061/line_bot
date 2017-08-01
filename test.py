473f94f7908de99586112538f5af9cc1776cc73c
Apie0419 <xu3mp6xjp6@gmail.com>
2017-08-01 21:31:03 +0800

    test

--- test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-# get channel_secret and channel_access_token from your environment variable
-@app.route("/", methods=['POST'])
-def show():
-    return "hello world"
-    
-
